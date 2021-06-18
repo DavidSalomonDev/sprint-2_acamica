3680213160e725fd2a9d3d339ac491b29ca5ce0f
davidChrome <davidsvmartinez@gmail.com>
2021-06-17 23:37:29 -0600

    💄 add sweetalert for dynamic interaction

--- src/components/Hotel/Hotel.jsx
+++ src/components/Hotel/Hotel.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import { LocationOn, LocalHotel, AttachMoneyOutlined } from '@material-ui/icons'
+import Swal from 'sweetalert2'
 import style from './Hotel.module.css'
 
 const Hotel = (props) => {
+
+	const alertaReservado = () => {
+		Swal.fire({
+			icon: 'success',
+			title: 'Hotel Reservado',
+			showConfirmButton: false,
+			timer: 1500
+		})
+	}
 
 	return (
 		<div className={style.hotel} id={props.data.slug}>
@@ -35,7 +45,12 @@ const Hotel = (props) => {
 					Precio: <AttachMoneyOutlined fontSize="large" /> {props.data.price}
 				</span>
 			</div>
-			<button className={style.btn}>Reservar</button>
+			<button
+				className={style.btn}
+				onClick={() => alertaReservado()}
+			>
+				Reservar
+			</button>
 		</div>
 	)
 }
--- src/components/inputs/Clear/Clear.jsx
+++ src/components/inputs/Clear/Clear.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 import style from '../../inputs/inputs.module.css'
 import { Delete } from '@material-ui/icons'
 
 const Clear = () => {
+	const alertaLimpiar = () => {
+		Swal.fire({
+			icon: 'info',
+			title: 'Limpio',
+			showConfirmButton: false,
+			timer: 500
+		})
+	}
 	return (
 		<div>
-			<button className={`${style.input} ${style.clear}`}>
+			<button
+				className={`${style.input} ${style.clear}`}
+				onClick={() => alertaLimpiar()}
+			>
 				<Delete fontSize="large" />	Limpiar
 			</button>
 		</div>
