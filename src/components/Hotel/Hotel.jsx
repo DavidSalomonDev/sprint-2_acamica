f64f924eda5055f7cc86885d875cfccfe60c9b4f
davidChrome <davidsvmartinez@gmail.com>
2021-06-17 23:19:47 -0600

    💄 edit hotel info and add material ui icons

--- src/components/Hotel/Hotel.jsx
+++ src/components/Hotel/Hotel.jsx
@@ -1,21 +1,40 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { LocationOn, LocalHotel, AttachMoneyOutlined } from '@material-ui/icons'
 import style from './Hotel.module.css'
 
 const Hotel = (props) => {
 
 	return (
 		<div className={style.hotel} id={props.data.slug}>
-			<img className={style.img} src={props.data.photo} alt={props.data.name} />
-			<h2 className={style.name}>{props.data.name}</h2>
-			<h3 className={style.startDate}>Desde el {props.data.availabilityFrom}</h3>
-			<h3 className={style.endDate}>Hasta el {props.data.availabilityTo}</h3>
-			<p className={style.description}>{props.data.description}</p>
-			<p className={style.location}>{props.data.city}, {props.data.country}</p>
+			<img
+				className={style.img}
+				src={props.data.photo}
+				alt={props.data.name}
+			/>
+			<h2 className={style.name}>
+				{props.data.name}
+			</h2>
+			<h3 className={style.startDate}>
+				Desde el {props.data.availabilityFrom}
+			</h3>
+			<h3 className={style.endDate}>
+				Hasta el {props.data.availabilityTo}
+			</h3>
+			<p className={style.description}>
+				{props.data.description}
+			</p>
+			<p className={style.location}>
+				<LocationOn /> {props.data.city}, {props.data.country}
+			</p>
 			<div className={style.wrapper}>
-				<span className={style.rooms}>{props.data.rooms} Habitaciones</span> <span className={style.price}>Precio: {props.data.price}</span>
+				<span className={style.rooms}>
+					<LocalHotel fontSize="large" />{props.data.rooms} Habitaciones
+				</span>
+				<span className={style.price}>
+					Precio: <AttachMoneyOutlined fontSize="large" /> {props.data.price}
+				</span>
 			</div>
-
 			<button className={style.btn}>Reservar</button>
 		</div>
 	)
--- src/components/inputs/Clear/Clear.jsx
+++ src/components/inputs/Clear/Clear.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import style from '../../inputs/inputs.module.css'
+import { Delete } from '@material-ui/icons'
 
 const Clear = () => {
 	return (
 		<div>
-			<button className={`${style.input} ${style.clear}`}>Limpiar</button>
+			<button className={`${style.input} ${style.clear}`}>
+				<Delete fontSize="large" />	Limpiar
+			</button>
 		</div>
 	)
 }
