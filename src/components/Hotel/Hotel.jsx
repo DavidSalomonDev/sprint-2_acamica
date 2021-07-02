e5b144231c1a9817a9a93a961d9ff1be26121788
salo <salo@david-salomon.com>
2021-07-02 10:48:36 -0600

    Revert ":recycle: refactor props names"
    This reverts commit 73ac9455

--- src/components/Hotel/Hotel.jsx
+++ src/components/Hotel/Hotel.jsx
@@ -1,22 +1,56 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { LocationOn, LocalHotel, AttachMoneyOutlined } from '@material-ui/icons'
+import Swal from 'sweetalert2'
 import style from './Hotel.module.css'
 
-const Hotel = ({data}) => {
+const Hotel = (props) => {
+
+	const alertaReservado = async () => {
+		await Swal.fire({
+			icon: 'success',
+			title: 'Hotel Reservado',
+			showConfirmButton: false,
+			timer: 1500
+		})
+	}
 
 	return (
-		<div className={style.hotel} id={data.slug}>
-			<img className={style.img} src={data.photo} alt={data.name} />
-			<h2 className={style.name}>{data.name}</h2>
-			<h3 className={style.startDate}>Desde el {data.availabilityFrom}</h3>
-			<h3 className={style.endDate}>Hasta el {data.availabilityTo}</h3>
-			<p className={style.description}>{data.description}</p>
-			<p className={style.location}>{data.city}, {data.country}</p>
+		<div className={style.hotel} id={props.data.slug}>
+			<img
+				className={style.img}
+				src={props.data.photo}
+				alt={props.data.name}
+			/>
+			<h2 className={style.name}>
+				{props.data.name}
+			</h2>
+			<h3 className={style.startDate}>
+				Desde el {props.data.availabilityFrom}
+			</h3>
+			<h3 className={style.endDate}>
+				Hasta el {props.data.availabilityTo}
+			</h3>
+			<p className={style.description}>
+				{props.data.description}
+			</p>
+			<p className={style.location}>
+				<LocationOn /> {props.data.city}, {props.data.country}
+			</p>
 			<div className={style.wrapper}>
-				<span className={style.rooms}>{data.rooms} Habitaciones</span> <span className={style.price}>Precio: {data.price}</span>
+				<span className={style.rooms}>
+					<LocalHotel fontSize="large" />{props.data.rooms} Habitaciones
+				</span>
+				<span className={style.price}>
+					Precio: <AttachMoneyOutlined fontSize="large" /> {props.data.price}
+				</span>
 			</div>
-
-			<button className={style.btn}>Reservar</button>
+			<button
+				className={style.btn}
+				onClick={() => alertaReservado()}
+			>
+				Reservar
+			</button>
 		</div>
 	)
 }
